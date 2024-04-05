612c8402e92c965de0c6935ebf1c64c65ab5d89e
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2024-04-05 00:09:21 -0600

    Creacion de clases para Layout y inicio de desarollo de clase
    Creacion de clases para Layout y inicio de desarollo de clase, Layout.jsx

---
+++ frontend/src/Components/Dashboard/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Layout.css';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    Sidebar
+  </div>
+);
+
+const Navbar = () => (
+  <div className="navbar">
+    Navbar
+  </div>
+);
