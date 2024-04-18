919a44b8c0f46b18d237094b43fe09e8f7a5fa83
José Gramajo <gra22907@uvg.edu.gt>
2024-04-18 17:18:10 -0600

    Cambios al estilo de botones y Sidebar

--- frontend/src/Components/Dashboard/Layout.jsx
+++ frontend/src/Components/Dashboard/Layout.jsx
@@ -1,14 +1,51 @@
 import React, { useState } from 'react';
 import './Layout.css';
 
-const Sidebar = ({ onChangeContent }) => (
-  <div className="sidebar">
-    <button onClick={() => onChangeContent("New Book")}>Nuevo Libro</button>
-    <button onClick={() => onChangeContent("Orders")}>Pedidos</button>
-    <button onClick={() => onChangeContent("Shipments")}>Envios</button>
-    <button onClick={() => onChangeContent("Stock")}>Inventario</button>
-  </div>
+// Define el componente de botón de la barra lateral que acepta ícono y texto
+const SidebarButton = ({ icon, text, onClick, isExpanded }) => (
+  <button className="sidebar-item" onClick={onClick}>
+    <span className="icon">{icon}</span>
+    {isExpanded && <span className="text">{text}</span>}
+  </button>
 );
+
+const Sidebar = ({ onChangeContent }) => {
+  // Estado para controlar la expansión de la Sidebar
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div
+      className={`sidebar ${isExpanded ? 'expanded' : 'collapsed'}`}
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+    >
+      <SidebarButton
+        icon={<img src="/icons8-book-30.png" alt="Nuevo Libro" />}
+        text="Nuevo Libro"
+        onClick={() => onChangeContent("Nuevo Libro")}
+        isExpanded={isExpanded}
+      />
+      <SidebarButton
+        icon={<img src="/icons8-order-history-30.png" alt="Pedidos" />}
+        text="Pedidos"
+        onClick={() => onChangeContent("Pedidos")}
+        isExpanded={isExpanded}
+      />
+      <SidebarButton
+        icon={<img src="/icons8-total-sales-30.png" alt="Ventas" />}
+        text="Ventas"
+        onClick={() => onChangeContent("Ventas")}
+        isExpanded={isExpanded}
+      />
+      <SidebarButton
+        icon={<img src="/icons8-trolley-30.png" alt="Inventario" />}
+        text="Inventario"
+        onClick={() => onChangeContent("Inventario")}
+        isExpanded={isExpanded}
+      />
+    </div>
+  );
+};
 
 const Navbar = () => (
   <div className="navbar">
@@ -26,7 +63,7 @@ const Layout = () => {
   // Estado para controlar el contenido activo
   const [activeContent, setActiveContent] = useState("Home");
 
-  // Función para cambiar el contenido
+  // Función para cambiar el contenido activo
   const handleContentChange = (content) => {
     setActiveContent(content);
   };
@@ -43,4 +80,3 @@ const Layout = () => {
 }
 
 export default Layout;
-
