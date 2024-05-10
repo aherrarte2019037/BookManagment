1d9f87ad72c768f39a9fd7be890ca154add56833
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2024-05-10 17:46:39 -0600

    cambios
    cambios

--- frontend/src/Components/AddOrder/AddOrder.jsx
+++ frontend/src/Components/AddOrder/AddOrder.jsx
@@ -47,43 +47,46 @@ export default function AddOrder() {
             await supabase.from('Books').update({ quantity: data.quantity - orderFormData.cantidad }).eq('id', data.id);
         }
     }
-
+    
     return (
-        <div className="order-form">
-          <h2>Pedido</h2>
-          <form onSubmit={handleOrderSubmit}>
-            <label htmlFor="tituloSolicitar">Título a solicitar</label>
-            <input value={orderFormData.titulo} onChange={handleOrderChange} type="text" id="tituloSolicitar" name="titulo" placeholder="Ingrese el título a solicitar" />
-    
-            <label htmlFor="cantidad">Cantidad</label>
-            <input value={orderFormData.cantidad} onChange={handleOrderChange} type="text" id="cantidad" name="cantidad" placeholder="Ingrese la cantidad" />
-    
-            <label htmlFor="nombreSolicitante">Nombre de colegio o distribuidor que lo solicita</label>
-            <input value={orderFormData.cliente} onChange={handleOrderChange} type="text" id="nombreSolicitante" name="cliente" placeholder="Ingrese el nombre de colegio o distribuidor" />
-    
-            <button type="submit">Enviar Pedido</button>
-          </form>
-    
-          <h2>Orden De Compra</h2>
-          <form>
-            <label htmlFor="temporada">Temporada</label>
-            <input type="text" id="temporada" name="temporada" placeholder="Ingrese la temporada" />
-    
-            <label htmlFor="vendedor">Vendedor</label>
-            <input type="text" id="vendedor" name="vendedor" placeholder="Ingrese el vendedor" />
-    
-            <label htmlFor="fecha">Fecha</label>
-            <input type="text" id="fecha" name="fecha" placeholder="Ingrese la fecha" />
-    
-            <label htmlFor="entrega">Entrega</label>
-            <input type="text" id="entrega" name="entrega" placeholder="Ingrese la entrega" />
-    
-            <label htmlFor="detalleCompra">Detalle De la compra</label>
-            <textarea id="detalleCompra" name="detalleCompra" placeholder="Ingrese el detalle de la compra"></textarea>
-    
-            <button type="submit">Confirmar Orden</button>
-          </form>
+        <div className="form-container">
+            <div className="custom-table">
+                <h3>Pedido</h3>
+                <form onSubmit={handleOrderSubmit}>
+                    <label htmlFor="tituloSolicitar">Título a solicitar</label>
+                    <input value={orderFormData.titulo} onChange={handleOrderChange} type="text" id="tituloSolicitar" name="titulo" placeholder="" />
+
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input value={orderFormData.cantidad} onChange={handleOrderChange} type="text" id="cantidad" name="cantidad" placeholder="" />
+
+                    <label htmlFor="nombreSolicitante">Nombre de colegio o distribuidor que lo solicita</label>
+                    <input value={orderFormData.cliente} onChange={handleOrderChange} type="text" id="nombreSolicitante" name="cliente" placeholder="" />
+
+                    <button type="submit">Enviar Pedido</button>
+                </form>
+            </div>
+
+            <div className="custom-table">
+                <h3>Orden De Compra</h3>
+                <form>
+                    <label htmlFor="temporada">Temporada</label>
+                    <input type="text" id="temporada" placeholder="" />
+
+                    <label htmlFor="vendedor">Vendedor</label>
+                    <input type="text" id="vendedor" placeholder="" />
+
+                    <label htmlFor="fecha">Fecha</label>
+                    <input type="text" id="fecha" placeholder="" />
+
+                    <label htmlFor="entrega">Entrega</label>
+                    <input type="text" id="entrega" placeholder="" />
+
+                    <label htmlFor="detalleCompra">Detalle De la compra:</label>
+                    <textarea id="detalleCompra" placeholder=""></textarea>
+
+                    <button type="submit">Confirmar Orden</button>
+                </form>
+            </div>
         </div>
-      );
-    }
-    
+    );
+}
--- frontend/src/Components/Dashboard/Layout.jsx
+++ frontend/src/Components/Dashboard/Layout.jsx
@@ -4,7 +4,6 @@ import { supabase } from '../../Utils/supabase';
 import { useNavigate } from 'react-router-dom';
 import AddBook from '../AddBook/AddBook';
 import AddOrder from '../AddOrder/AddOrder';
-import ProfileConfig from '../ProfileConfig/ProfileConfig';
 
 
 const SidebarButton = ({ icon, text, onClick, isExpanded }) => (
@@ -18,7 +17,6 @@ const Sidebar = ({ onChangeContent }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    
     <div
       className={`sidebar ${isExpanded ? 'expanded' : 'collapsed'}`}
       onMouseEnter={() => setIsExpanded(true)}
@@ -88,25 +86,6 @@ const Navbar = ({ onChangeContent }) => {
 const Content = ({ activeContent }) => {
   const renderContent = () => {
     switch (activeContent) {
-      case "Home":
-        return (
-          <div className="new-book-form-home">
-            <b><h1>Informacion General</h1></b>
-            <form>
-            <label htmlFor="editorial">¿Quienes somos?</label>
-              <p>La Editorial Textos Escolares es una empresa guatemalteca líder en la producción y distribución de materiales educativos. Su enfoque en la innovación y la calidad la ha posicionado como un referente en el sector editorial. Con valores de ética, colaboración y compromiso social, la empresa busca contribuir al desarrollo integral de las comunidades educativas. Su visión es expandirse a nivel nacional e internacional, manteniendo su pasión por la educación y la mejora continua.</p>
-              <label htmlFor="editorial">Mision</label>
-              <p>Facilitar el acceso a una educación de calidad mediante la producción y distribución de materiales educativos innovadores y relevantes, comprometiéndonos con la excelencia en cada etapa de nuestro trabajo, desde la creación hasta la entrega final, para contribuir al desarrollo integral de las comunidades educativas.</p>
-              <label htmlFor="editorial">Vision</label>
-              <p>Nuestra visión es dejar una huella indeleble en la experiencia de conocimiento en los estudiantes, que sientan los libros de texto como un apoyo, un amigo de aprendizaje, donde puedan encontrar todo un mundo de conocimiento y experiencias, que los invite a querer aprender más, a experimentar en su propia vida las diferentes áreas que se puedan presentar.</p>
-              <label htmlFor="editorial">Valores</label>
-              <p>Compromiso con la calidad educativa: Nos comprometemos a ofrecer materiales educativos de alta calidad que promuevan el aprendizaje significativo y el desarrollo integral de los estudiantes.<br /><br />
-Innovación constante: Buscamos constantemente nuevas formas de mejorar y adaptarnos a las necesidades cambiantes de las comunidades educativas, impulsando la innovación en cada aspecto de nuestro trabajo.<br /><br />
-Ética y responsabilidad: Actuamos con integridad y responsabilidad en todas nuestras acciones, manteniendo altos estándares éticos en nuestras relaciones con empleados, autores, clientes y proveedores.<br /><br />
-Colaboración y diversidad: Valoramos la diversidad de perspectivas y experiencias, fomentando un entorno de colaboración donde cada voz sea escuchada y respetada.<br /><br />
-Excelencia y profesionalismo: Nos esforzamos por alcanzar la excelencia en todo lo que hacemos, manteniendo altos estándares de profesionalismo y dedicación en cada proyecto que emprendemos.</p>
-            </form>
-          </div>); ;
       case "Nuevo Libro":
         return <AddBook />;
 
@@ -143,13 +122,18 @@ Excelencia y profesionalismo: Nos esforzamos por alcanzar la excelencia en todo 
 
               <button type="submit">Agregar Producto</button>
             </form>
+          </div>);
+      case "Configuración":
+        return (
+          <div>
+            <h2>Configuración</h2>
+            {/* Opciones de configuración */}
           </div>
         );
-
-      case "Configuración":
-        return <ProfileConfig />;
       case "Cerrar Sesión":
+        // Manejar la lógica de cerrar sesión
         return <h2>Cerrando sesión...</h2>;
+      // Agrega más casos para otros tipos de contenido si es necesario
       default:
         return <div>{activeContent}</div>;
     }
