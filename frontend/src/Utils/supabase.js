7a9698ea6e127032081447ff6118465c7fd492ef
Angel <angelherrarte3@gmail.com>
2024-05-10 15:44:41 -0600

    Fix env

--- frontend/src/Utils/supabase.js
+++ frontend/src/Utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = 'https://twbcdzatgncpqiomnnda.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3YmNkemF0Z25jcHFpb21ubmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwOTgxNTYsImV4cCI6MjAyNzY3NDE1Nn0.o-8iFiprRac5eYsghljeiI6Syj5bPO1jkj7XAXFWUZ0';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
