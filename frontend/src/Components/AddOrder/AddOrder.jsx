c64d0f85d2e9091f82751bfeb5c97dfd7c43dfd9
José Gramajo <gra22907@uvg.edu.gt>
2024-07-31 21:20:32 -0600

    Mejoras en el front del componente AddOrder

--- frontend/src/Components/AddOrder/AddOrder.jsx
+++ frontend/src/Components/AddOrder/AddOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './AddOrder.css'; // Importa tus estilos CSS
+import './AddOrder.css';
 
 export default function AddOrder() {
     const [formData, setFormData] = useState({
@@ -31,37 +31,53 @@ export default function AddOrder() {
 
     return (
         <div className="form-container">
-            <h2>Pedido</h2>
-            <form onSubmit={handleSubmit}>
-                <label className="label">Título a solicitar</label>
-                <input className="input" type="text" name="titulo" value={formData.titulo} onChange={handleChange} />
-
-                <label className="label">Cantidad</label>
-                <input className="input" type="number" name="cantidad" value={formData.cantidad} onChange={handleChange} />
-
-                <label className="label">Nombre de colegio o distribuidor que lo solicita</label>
-                <input className="input" type="text" name="colegio" value={formData.colegio} onChange={handleChange} />
-
-                <label className="label">Orden De Compra</label>
-                <input className="input" type="text" name="ordenCompra" value={formData.ordenCompra} onChange={handleChange} />
-
-                <label className="label">Temporada</label>
-                <input className="input" type="text" name="temporada" value={formData.temporada} onChange={handleChange} />
-
-                <label className="label">Vendedor</label>
-                <input className="input" type="text" name="vendedor" value={formData.vendedor} onChange={handleChange} />
-
-                <label className="label">Fecha</label>
-                <input className="input" type="date" name="fecha" value={formData.fecha} onChange={handleChange} />
-
-                <label className="label">Entrega</label>
-                <input className="input" type="text" name="entrega" value={formData.entrega} onChange={handleChange} />
-
-                <label className="label">Detalle De la compra</label>
-                <textarea className="input" name="detalleCompra" value={formData.detalleCompra} onChange={handleChange} />
-
-                <button type="submit" className="button">Confirmar Orden</button>
-            </form>
+            <div className="new-book-form">
+                <h2>Agregar Orden</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label className="label">Título a solicitar</label>
+                        <input className="input" type="text" name="titulo" value={formData.titulo} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Cantidad</label>
+                        <input className="input" type="number" name="cantidad" value={formData.cantidad} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Nombre de colegio o distribuidor que lo solicita</label>
+                        <input className="input" type="text" name="colegio" value={formData.colegio} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Orden De Compra</label>
+                        <input className="input" type="text" name="ordenCompra" value={formData.ordenCompra} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Temporada</label>
+                        <input className="input" type="text" name="temporada" value={formData.temporada} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Vendedor</label>
+                        <input className="input" type="text" name="vendedor" value={formData.vendedor} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Fecha</label>
+                        <input className="input" type="date" name="fecha" value={formData.fecha} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Entrega</label>
+                        <input className="input" type="text" name="entrega" value={formData.entrega} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label className="label">Detalle De la compra</label>
+                        <textarea className="input" name="detalleCompra" value={formData.detalleCompra} onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="cta">
+                        <span>Agregar Orden</span>
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                            <polyline points="6 12 10 16 18 8"></polyline>
+                        </svg>
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
